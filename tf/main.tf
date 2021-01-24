4532e8e78e0084875420ab81b25bc9b04967382f
JustAddRobots <justaddrobots@icloud.com>
2021-01-23 23:55:23 -0800

    ISSUE-012: Added kubernetes, jenkins smee, refactored for configs

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,6 @@
 variable "password" {
     default: "packer"
+    ami_url: "file:///tmp/packer/controller/centos-7.9-x86_64-controller.vmx"
 }
 
 provider "vix" {
@@ -8,13 +9,19 @@ provider "vix" {
     clone_type = "full"
 }
 
-resource "vix_vm" "worker0" {
-    name = "worker0"
-    description = "Terraform VMWare Fusion worker0"
+resource "vix_vm" "controller" {
+    cpus = 2
+    description = "Terraform VMWare Fusion controller"
+    gui = false
     image {
+        url = "${var.ami_url}"
+        checksum = ""
+        checksum_type = ""
 
     }
-
+    name = "worker0"
+    memory = "16.0gib"
     network_adapter {
         type = "bridged"
     }
+}
